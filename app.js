1c7a8d217e7ae355fe72f72b679e6ba911037bae
codeCrafts <codecraftsman12@gmail.com>
2025-01-08 00:00:15 +0600

    new changes

--- app.js
+++ app.js
@@ -3,16 +3,25 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const userRouter = require("./routes/user.route");
-require("./config/user.db")
+require("./config/user.db");
 
+// Add timeout middleware first
+app.use((req, res, next) => {
+  res.setTimeout(25000, () => {
+    res.status(504).send('Request Timeout');
+  });
+  next();
+});
+
+// Your existing middleware
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/api",userRouter);
+app.use("/api", userRouter);
 
-// base route
+// Your existing routes
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/./views/index.html");
+  res.sendFile(_dirname + "./views/index.html");
 });
 
 // handle route error
