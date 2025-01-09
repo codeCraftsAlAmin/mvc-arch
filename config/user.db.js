564ea2b107df794415698acae15cce895fe5c1ea
codeCrafts <codecraftsman12@gmail.com>
2025-01-09 15:28:45 +0600

    new changes

--- config/user.db.js
+++ config/user.db.js
@@ -3,14 +3,6 @@ const mongoose = require("mongoose");
 const config = require("./user.config");
 const dbUrl = config.db.url;
 
-// mongoose
-//   .connect(dbUrl)
-//   .then(() => console.log("db is connected"))
-//   .catch((error) => {
-//     console.log("db isn't connected", error.message);
-//     process.exit(1);
-//   });
-
 const connectDb = async () => {
   try {
     await mongoose.connect(dbUrl);
